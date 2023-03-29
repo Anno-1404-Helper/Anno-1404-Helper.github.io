a176fd53f8e65a995b533212bac1bbf4b05429a8
vassdeniss <vassdeniss@gmail.com>
2023-03-29 19:31:19 +0300

    Made layout / settings page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import page from './lib/page.mjs';
 
 import { addRender } from './middlewares/render.js';
+import { addSession } from './middlewares/session.js';
 
 import { renderSettings } from './views/settings.js';
 import { renderIslands } from './views/islands.js';
 import { renderLogin } from './views/login.js';
 
 page(addRender);
+page(addSession);
 
 page('/index.html', '/');
 page('/', renderIslands);
--- src/views/settings.js
+++ src/views/settings.js
@@ -1,7 +1,41 @@
-import { html } from '../lib/lit-html.js';
+import { html, nothing } from '../lib/lit-html.js';
 
 export function renderSettings(ctx) {
-  ctx.render(settignsTemplate());
+  ctx.render(settignsTemplate(ctx.user));
 }
 
-const settignsTemplate = () => html`<h1>Settings Page</h1>`;
+const settignsTemplate = (user) => html`<h1>Settings Page</h1>
+  <section class="main">
+    ${user
+      ? nothing
+      : html`<div>
+            <a class="link" href="/login">Sign in</a> to enable cloud sync
+          </div>
+          <br />`}
+
+    <table>
+      <thead>
+        <tr>
+          <th>Game Name</th>
+          <th>Controls</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td colspan="2">No games recorded!</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="2">
+            <form>
+              <input type="text" name="name" placeholder="New Game Name" />
+              <button class="btn">
+                <i class="fa-solid fa-plus"></i> Create Game
+              </button>
+            </form>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </section>`;
